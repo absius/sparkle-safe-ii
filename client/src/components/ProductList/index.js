4dfcc6b561fcdcde06114eb46e0f052c42a38d1b
Boba Fett <thomascupchurch@gmail.com>
2021-11-14 11:32:47 -0600

    adds queries.js to client/utils

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -5,48 +5,44 @@ import JewelryItem from "../JewelryItem";
 import { QUERY_JEWELRY } from "../../utils/queries";
 import spinner from "../../assets/spinner.gif";
 
-function JewelryList({ currentCategory }) {
+function JewelryList() {
   const { loading, data } = useQuery(QUERY_JEWELRY);
 
   const jewelryList = data?.jewelryList || [];
 
   function filterJewelryList() {
     if (jewelryList.length) {
-      return jewelryList;
-    } else {
-      return "You have not added any jewelry yet.";
+      return (
+        <div className="my-2">
+          <h2>My Jewelry</h2>
+          {jewelryList.length ? (
+            <div className="flex-row">
+              {filterJewelryList().map((jewelryItem) => (
+                <JewelryItem
+                  key={jewelryItem._id}
+                  _id={jewelryItem._id}
+                  jewelryName={jewelryItem.jewelryName}
+                  description={jewelryItem.description}
+                  jewelryPrice={jewelryItem.jewelryPrice}
+                  assessedValue={jewelryItem.assessedValue}
+                  jewelryAssessor={jewelryItem.jewelryAssessor}
+                  purchasedDate={jewelryItem.purchasedDate}
+                  jewelryWarranty={jewelryItem.jewelryWarranty}
+                  servicedDate={jewelryItem.servicedDate}
+                  jewelryPhoto={jewelryItem.jewelryPhoto}
+                  receiptPhoto={jewelryItem.receiptPhoto}
+                  createdAt={jewelryItem.createdAt}
+                />
+              ))}
+            </div>
+          ) : (
+            <h3>You haven't added any jewelry yet!</h3>
+          )}
+          {loading ? <img src={spinner} alt="loading" /> : null}
+        </div>
+      );
     }
   }
-
-  return (
-    <div className="my-2">
-      <h2>My Jewelry</h2>
-      {jewelryList.length ? (
-        <div className="flex-row">
-          {filterJewelryList().map((jewelryItem) => (
-            <JewelryItem
-              key={jewelryItem._id}
-              _id={jewelryItem._id}
-              jewelryName={jewelryItem.jewelryName}
-              description={jewelryItem.description}
-              jewelryPrice={jewelryItem.jewelryPrice}
-              assessedValue={jewelryItem.assessedValue}
-              jewelryAssessor={jewelryItem.jewelryAssessor}
-              purchasedDate={jewelryItem.purchasedDate}
-              jewelryWarranty={jewelryItem.jewelryWarranty}
-              servicedDate={jewelryItem.servicedDate}
-              jewelryPhoto={jewelryItem.jewelryPhoto}
-              receiptPhoto={jewelryItem.receiptPhoto}
-              createdAt={jewelryItem.createdAt}
-            />
-          ))}
-        </div>
-      ) : (
-        <h3>You haven't added any products yet!</h3>
-      )}
-      {loading ? <img src={spinner} alt="loading" /> : null}
-    </div>
-  );
 }
 
-export default ProductList;
+export default JewelryList;
