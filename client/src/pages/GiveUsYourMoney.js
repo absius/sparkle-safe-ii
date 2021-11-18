a4b5b8e82465a115057bcfc775aa97dd5e6f3a48
A'Darius Ellis <85079903+gadaell@users.noreply.github.com>
2021-11-18 05:31:01 -0600

    updated readme and stripe file

--- client/src/pages/GiveUsYourMoney.js
+++ client/src/pages/GiveUsYourMoney.js
@@ -3,12 +3,48 @@ import { Link } from "react-router-dom";
 import { PaymentElement } from "@stripe/react-stripe-js";
 
 const GiveUsYourMoney = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    // We don't want to let default form submission happen here,
+    // which would refresh the page.
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+    const result = await stripe.confirmPayment({
+      //`Elements` instance that was used to create the Payment Element
+      elements,
+      confirmParams: {
+        return_url: "https://sparkle-safe.com/giveUsYourMoney",
+      },
+    });
+    if (result.error) {
+      // Show error to your customer (e.g., payment details incomplete)
+      console.log(result.error.message);
+    } else {
+      // Your customer will be redirected to your `return_url`. For some payment
+      // methods like iDEAL, your customer will be redirected to an intermediate
+      // site first to authorize the payment, then redirected to the `return_url`.
+    }
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <PaymentElement />
-      <button>Submit</button>
+      <button disabled={!stripe}>Submit</button>
     </form>
   );
+
+  // return (
+  //   <form>
+  //     <PaymentElement />
+  //     <button>Submit</button>
+  //   </form>
+  // );
 };
 
 export default GiveUsYourMoney;
