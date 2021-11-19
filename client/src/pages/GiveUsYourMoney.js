2ef280f2092307a38b99b73d512e027fceb0e2e4
A'Darius Ellis <85079903+gadaell@users.noreply.github.com>
2021-11-18 19:41:57 -0600

    fixed mongoose

--- client/src/pages/GiveUsYourMoney.js
+++ client/src/pages/GiveUsYourMoney.js
@@ -1,47 +1,18 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import { PaymentElement } from "@stripe/react-stripe-js";
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+const stripePromise = loadStripe(process.env.PUBLISHABLE_KEY);
 
-// const GiveUsYourMoney = () => {
-//   const handleSubmit = async (event) => {
-//     // We don't want to let default form submission happen here,
-//     // which would refresh the page.
-//     event.preventDefault();
+const GiveUsYourMoney = () => {
+  return (
+    <>
+      <h2>Payment Options</h2>
+      <form action="/create-checkout-session" method="POST">
+        <button type="submit">Donations</button>
+      </form>
+      <Elements stripe={stripePromise}></Elements>
+    </>
+  );
+};
 
-//     if (!stripe || !elements) {
-//       // Stripe.js has not yet loaded.
-//       // Make sure to disable form submission until Stripe.js has loaded.
-//       return;
-//     }
-//     const result = await stripe.confirmPayment({
-//       //`Elements` instance that was used to create the Payment Element
-//       elements,
-//       confirmParams: {
-//         return_url: "https://sparkle-safe.com/giveUsYourMoney",
-//       },
-//     });
-//     if (result.error) {
-//       // Show error to your customer (e.g., payment details incomplete)
-//       console.log(result.error.message);
-//     } else {
-//       // Your customer will be redirected to your `return_url`. For some payment
-//       // methods like iDEAL, your customer will be redirected to an intermediate
-//       // site first to authorize the payment, then redirected to the `return_url`.
-//     }
-//   };
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <PaymentElement />
-//       <button disabled={!stripe}>Submit</button>
-//     </form>
-//   );
-
-//   // return (
-//   //   <form>
-//   //     <PaymentElement />
-//   //     <button>Submit</button>
-//   //   </form>
-//   // );
-// };
-
-// export default GiveUsYourMoney;
+export default GiveUsYourMoney;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
-
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
-const db = require('./config/connection');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const stripe = require("stripe")(
+  "sk_test_51JxI5QGZoMRsRH0yG4Cbcjm5tC7NCIYZ3TnQfUroe674bx43w8sI8WAFSLnqbViZsrpbVgR96hczDffx78YZD0fM00DnqPXrqP"
+);
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  context: authMiddleware,
 });
 
 server.applyMiddleware({ app });
@@ -19,17 +21,32 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-db.once('open', () => {
+app.post("/giv", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+        price: 50,
+      },
+    ],
+    mode: "payment",
+    payment_method_types: ["card"],
+    success_url: "https://example.com/success",
+    cancel_url: "https://example.com/cancel",
+  });
+
+  res.redirect(303, session.url);
+});
+
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
